11abe040d0e21ed7b18f7a76a1e404be10fa8c36
david <david@debian>
2016-04-18 21:02:47 +0100

    starting...

--- db/src/topdir/bin/run.sh
+++ db/src/topdir/bin/run.sh
@@ -1,7 +1,7 @@
 NOW=`date +%Y%m%d-%H.%M.%S`
 
-/c/q/w32/q.exe code/tick.q -p 10000 > ./logs/$NOW"_ticker.log" 2>&1 &
+$QEXE code/tick.q -p 127.0.0.1:10000 > ./logs/$NOW"_ticker.log" 2>&1 &
 
-/c/q/w32/q.exe data -p 10002 > ./logs/$NOW"_history.log" 2>&1 &
+$QEXE data -p 127.0.0.1:10002 > ./logs/$NOW"_history.log" 2>&1 &
 
-/c/q/w32/q.exe code/rdb.q -source ::10000 -p 10001 > ./logs/$NOW"_realtime.log" 2>&1 &
+$QEXE code/rdb.q -source ::10000 -p 127.0.0.1:10001 > ./logs/$NOW"_realtime.log" 2>&1 &
